c3d4acadeb7e5e4b417434f46816201f2301eb2e
C. L <amautacelt@gmail.com>
2020-12-31 06:14:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  final_results = data.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_key, nammes|
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+        end
+        if !final_array[name]
+          
+
 end
